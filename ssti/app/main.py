81cfc88871cbf5446928aa1901c41665160eba66
knockie <knockdown09@gmail.com>
2019-01-31 23:40:59 +0700

    Update main.py

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, render_template_string
+import re
 
 app = Flask(__name__)
 
@@ -6,21 +7,39 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-	return render_template('index.html')
+        return render_template('index.html')
 
 
 @app.route("/unsafe")
 def unsafe_ssti():
 
-	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
+        person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
 
-	if person['name'] is None:
-		person['name'] = 'world!'
+        if person['name'] is None:
+                person['name'] = 'world!'
 
-	body = "Name: %s" % person['name']
+        body = "Name: %s" % person['name']
 
-	return render_template_string(body, person=person)
+        return render_template_string(body, person=person)
+@app.route("/safe")
+def safe_ssti():
+        person = {'name': request.args.get('whoami')}
 
+        if person['name'] is None:
+                 person['name'] = 'world!'
+        body = 'True name: %s' % re.sub('[^A-Za-z]', '', person['name'])
+        return body
+@app.route("/truesafe")
+def truesafe_ssti():
+
+        person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
+
+        if person['name'] is None:
+                person['name'] = 'world!'
+
+        body = "Name:  {{person['name']}}"
+
+        return render_template_string(body, person=person)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=80)
+        app.run(host='0.0.0.0', debug=True, port=80)
