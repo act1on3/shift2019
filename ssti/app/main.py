2ccf52e196035fa05a04f62ce0e9fe2b044c03d1
act1on3 <>
2019-02-01 13:23:02 +0700

    ssti: add string.Template and pystache methods

--- ssti/app/main.py
+++ ssti/app/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template, render_template_string
+from string import Template
+import pystache
 
 app = Flask(__name__)
 
@@ -35,5 +37,31 @@ def safe_ssti():
 	return render_template_string(body, person=person)
 
 
+@app.route("/unsafe2")
+def unsafe2_ssti():
+
+	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
+
+	if person['name'] is None:
+		person['name'] = 'world!'
+
+	body = Template('Name: %s' % person['name'])
+
+	return body.safe_substitute()
+
+
+@app.route("/unsafe3")
+def unsafe3_ssti():
+
+	person = {'name': request.args.get('whoami'), 'secret': 'You win, master jedi!'}
+
+	if person['name'] is None:
+		person['name'] = 'world!'
+
+	body = "Name: %s" % person['name']
+
+	return pystache.render(body)
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=80)
+	app.run(host='0.0.0.0', debug=True, port=1337)
